2dd1a86877e408c66e2a3b2295acf0ea74a10e6e
Umar Arshad <elliotalderson4444@gmail.com>
2022-12-19 20:37:36 +0500

    added the animation

---
+++ src/components/ReuseableCompAbstraction.tsx
@@ -0,0 +1,16 @@
+import ProjectReuseable from './ProjectReuseable'
+
+function ReuseableCompAbstraction() {
+    return (
+        <div>
+
+            <ProjectReuseable image="/work-1.jpg.webp" heading="portfolio" title="portfolio website with full functionalities" description="this is the portfolio website with fully responsive and latest techniques and techs." url="https://umar-arshad-portfolio.netlify.app/" />
+            <ProjectReuseable image="/work-2.jpg.webp" heading="tesla clone" title="tesla clone website with full responsiveness" description="this is the fully customized website with fully responsive and latest techniques and techs." order url="https://react-tesla-clone-project.netlify.app/" />
+            <ProjectReuseable image="/work-3.jpg.webp" heading="Ecommerce" title="ecommerce website with full functionalities" description="this is the ecommerce website with fully responsive and latest techniques and techs." url="https://adonis-ecommerce-store.netlify.app/" />
+            <ProjectReuseable image="/work-4.jpg.webp" heading="portfolio" title="portfolio website with full functionalities" description="this is the mern stack website with fully responsive and latest techniques and techs." order url="https://react-ts-portfolio4.netlify.app" />
+            <ProjectReuseable image="/work-5.jpg.webp" heading="starbucks" title="starbucks website with full functionalities" description="this is the starbucks website with fully responsive and latest techniques and techs." url="https://starbucks-portfolio4.netlify.app" />
+        </div>
+    )
+}
+
+export default ReuseableCompAbstraction
